a98aa71aaef0879fa247e4c6a08505e792979753
patrick-codes <khofilyndon2018@gmail.com>
2025-04-22 19:19:15 +0000

    added receipt page

--- salonapp_client/lib/presentation/appointments/pages/receipt_page.dart
+++ salonapp_client/lib/presentation/appointments/pages/receipt_page.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:salonapp_client/helpers/colors/color_constants.dart';
+import '../../../helpers/colors/widgets/custom_button.dart';
 
 class ReceiptPage extends StatefulWidget {
   final String id;
@@ -19,182 +19,206 @@ class _ReceiptPageState extends State<ReceiptPage> {
   Widget build(BuildContext context) {
     print(widget.id);
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: 90,
+        width: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            width: 1,
+            color: Colors.grey.shade100,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Center(
+            child: CustomButton(
+              text: 'Checkout',
+              onpressed: () {},
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ),
       backgroundColor: secondaryBg,
       body: SafeArea(
-        child: Column(
-          children: [
-            DottedBorder(
-              strokeWidth: 1,
-              dashPattern: const [6, 4],
-              color: primaryColor,
-              borderType: BorderType.RRect,
-              radius: const Radius.circular(5),
-              child: Container(
-                height: 300,
-                padding: const EdgeInsets.all(15),
-                width: 350,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Colors.white,
-                  boxShadow: const [
-                    BoxShadow(
-                      blurRadius: 5,
-                      spreadRadius: 0.5,
-                      color: Color.fromARGB(255, 234, 233, 233),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Shipping Weight",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black45),
-                        ),
-                        Text(
-                          "GHS weightFee",
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              DottedBorder(
+                strokeWidth: 1,
+                dashPattern: const [6, 4],
+                color: iconGrey,
+                borderType: BorderType.RRect,
+                radius: const Radius.circular(5),
+                child: Container(
+                  height: 600,
+                  padding: const EdgeInsets.all(15),
+                  width: 350,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        blurRadius: 5,
+                        spreadRadius: 0.5,
+                        color: Color.fromARGB(255, 234, 233, 233),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Shipping Weight",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(
                                     fontWeight: FontWeight.bold,
-                                  ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Subtotal",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black45),
-                        ),
-                        Text(
-                          "GHS",
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Shipping & Handling",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black45),
-                        ),
-                        Text(
-                          "GHS shippingFee",
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Customs/imports/duties/taxes",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black45),
-                        ),
-                        Text(
-                          "Not included",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodySmall!
-                              .copyWith(color: Colors.red),
-                        ),
-                      ],
-                    ),
-
-                    ///
-                    const SizedBox(height: 25),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Icon(Icons.check_box_outline_blank_outlined,
-                            color: Colors.black45),
-                        const SizedBox(width: 5),
-                        SizedBox(
-                          width: 273,
-                          child: Column(
-                            children: [
-                              Text(
-                                overflow: TextOverflow.visible,
-                                softWrap: true,
-                                "I agree that customs and import Duties & Taxes will be collected by the courier company at the time of delivery.",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium!
-                                    .copyWith(fontSize: 12),
-                              ),
-                            ],
+                                    color: Colors.black45),
                           ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 30),
-                    const Divider(
-                      height: 5,
-                    ),
-                    const SizedBox(height: 12),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Grand Total",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "GHS total",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium!
-                              .copyWith(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 12),
-                    const Divider(
-                      height: 5,
-                    ),
-                  ],
+                          Text(
+                            "GHS weightFee",
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Subtotal",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black45),
+                          ),
+                          Text(
+                            "GHS",
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Shipping & Handling",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black45),
+                          ),
+                          Text(
+                            "GHS shippingFee",
+                            style:
+                                Theme.of(context).textTheme.bodySmall!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Customs/imports/duties/taxes",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black45),
+                          ),
+                          Text(
+                            "Not included",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodySmall!
+                                .copyWith(color: Colors.red),
+                          ),
+                        ],
+                      ),
+
+                      ///
+                      const SizedBox(height: 25),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Icon(Icons.check_box_outline_blank_outlined,
+                              color: Colors.black45),
+                          const SizedBox(width: 5),
+                          SizedBox(
+                            width: 273,
+                            child: Column(
+                              children: [
+                                Text(
+                                  overflow: TextOverflow.visible,
+                                  softWrap: true,
+                                  "I agree that customs and import Duties & Taxes will be collected by the courier company at the time of delivery.",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium!
+                                      .copyWith(fontSize: 12),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 30),
+                      const Divider(
+                        height: 5,
+                      ),
+                      const SizedBox(height: 12),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Grand Total",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "GHS total",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 12),
+                      const Divider(
+                        height: 5,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

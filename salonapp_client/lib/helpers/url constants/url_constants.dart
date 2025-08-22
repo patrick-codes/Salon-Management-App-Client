3804c6e1b91228d06d8f3ed91b13f6b3fb66cdf1
patrick-codes <khofilyndon2018@gmail.com>
2025-08-22 08:42:17 +0000

    added cloudinary

--- salonapp_client/lib/helpers/url constants/url_constants.dart
+++ salonapp_client/lib/helpers/url constants/url_constants.dart
@@ -1,20 +1,26 @@
-class UrlConstants {
-  static String baseUrl = "https://fakestoreapi.com";
+String baseUrl = "https://fakestoreapi.com";
 
-  //Products EndPoints
-  static String getallproducts = "https://fakestoreapi.com/products";
-  static String getsingleproduct = "$baseUrl/products/1";
+//Products EndPoints
+String getallproducts = "https://fakestoreapi.com/products";
+String getsingleproduct = "$baseUrl/products/1";
 
-  //Category EndPoints
-  static String getallcategory = "$baseUrl/products/categories";
-  static String getsinglecategory = "$baseUrl/products/jewelry";
+//Category EndPoints
+String getallcategory = "$baseUrl/products/categories";
+String getsinglecategory = "$baseUrl/products/jewelry";
 
-  //Cart EndPoints
-  static String getallcarts = "$baseUrl/products/carts";
-  static String getsinglecart = "$baseUrl/products/carts/5";
+//Cart EndPoints
+String getallcarts = "$baseUrl/products/carts";
+String getsinglecart = "$baseUrl/products/carts/5";
 
-  //Google Drive API
-  static String clientId =
-      "942271446904-59v7331klq1k7j25tgp0q3hn63g8g05g.apps.googleusercontent.com";
-  static String apiKey = "AIzaSyAMQ8djnTHcWF00DXZ9W28Pdl_ALN8qboY";
-}
+//Google Drive API
+String clientId =
+    "942271446904-59v7331klq1k7j25tgp0q3hn63g8g05g.apps.googleusercontent.com";
+String apiKey = "AIzaSyAMQ8djnTHcWF00DXZ9W28Pdl_ALN8qboY";
+
+//Cloudinary
+const String cloudName = "dknucbdrg";
+const String cloudinaryApiKey = "767675827389346";
+const String apiSecret = "cqZPQ_vxXzmsqgBoQ9RemHzr8m8";
+const String cloudinaryUrl =
+    "cloudinary://$cloudinaryApiKey:$apiSecret@dknucbdrg";
+const String uploadPreset = "salonapp";
